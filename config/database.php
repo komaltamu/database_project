291178e9a4c16448a22899de61cd2842aeb9834b
komalilyas <komal.ilyas9@gmail.com>
2023-03-20 15:20:01 -0500

    pgsql

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-3-218-171-44.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dd106i0f6h9els'),
+            'username' => env('DB_USERNAME', 'vtwrgluvmqhbtt'),
+            'password' => env('DB_PASSWORD', '65a9645afb5969f3d13b66637706e959cf5460108e7cea65de519c8c359837e1'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
